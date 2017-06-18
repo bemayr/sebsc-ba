4aa284bfe92212e3372b648e0e1624cebaf00385
Bernhard Mayr <bernhard@mayr.io>
2017-06-18 19:19:14 +0200

    major folder restructuring

--- listings/npm-deploy.sh
+++
@@ -1,4 +0,0 @@
-# change to service directory
-cd ~/code/exciting-service
-# push to production
-git push origin +master:deploy-production
---
+++ theoretical-part/listings/greet.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from Docker!");
+    return 0;
+}
