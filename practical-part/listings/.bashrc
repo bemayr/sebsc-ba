10baa841e655bfa1e84771c56b8231618fa618b9
Bernhard Mayr <bernhard@mayr.io>
2017-06-24 09:14:20 +0200

    more content

---
+++ practical-part/listings/.bashrc
@@ -0,0 +1,6 @@
+# --- configuring the prompt to look like "dev@docker:[<current-directory>]>" --- #
+export PS1="dev@docker:\e[1;36m[\W]\e[m> "
+
+# --- printing a welcome message --- #
+printf '\nYour Webdevelopment-Environment is ready to go - Start Coding... ;)\n\n'
+
---
+++ practical-part/listings/create-image.ps1
@@ -0,0 +1,68 @@
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory=$true, HelpMessage="Enter the specified node-version (e.g. 6.10.2) from https://nodejs.org/en/download/releases/.")]
+  [String]$node_version,
+  
+  [Switch]$silent,
+  
+  [Switch]$publish
+)
+
+# ===== CONSTANTS =====
+$REPOSITORYNAME = "softaware/webdev"
+$IMAGEPATH = "./images/"
+$DOCKERFILEROOTPATH = "$($IMAGEPATH)Dockerfile"
+$DISTRIBUTIONS = @("alpine","debian")
+
+function Generate-Image {
+  param(
+    [String]$node_version,
+    [String]$distribution,
+    [Boolean]$silent,
+    [Boolean]$publish
+  )
+
+  $generatedfile = $IMAGEPATH + "Dockerfile.gen"
+
+  $dockerfile = $DOCKERFILEROOTPATH + "." + $distribution
+  $tag = $distribution + "-" + $node_version
+  $imagename = $REPOSITORYNAME + ":" + $tag
+
+  $template = Get-Content $dockerfile -Raw
+  $generated = $template.Replace("{{ node_version }}", $node_version)
+  $generated | Out-File -Encoding UTF8 $generatedfile
+
+  $command = {docker image build (&{If($silent) {"--quiet"}}) --force-rm -f $generatedfile -t $imagename $IMAGEPATH}
+  if (!$silent) {& $command} else {& $command | Out-Null}
+  Remove-Item $generatedfile
+
+  "$($imagename) created successfully"
+
+  if ($publish) {
+    Publish-Image $imagename
+  }
+}
+
+function Publish-Image {
+  param(
+    [String]$imagename
+  )
+  $command = {docker push $imagename}
+  if (!$silent) {& $command} else {& $command | Out-Null}
+
+  "$($imagename) published successfully"
+}
+
+function Generate-Images {
+  param(
+    [String]$node_version,
+    [String[]]$distribution,
+    [Boolean]$silent,
+    [Boolean]$publish
+  )
+  foreach ($distribution in $distribution) {
+    Generate-Image $node_version $distribution $silent $publish
+  }
+}
+
+Generate-Images $node_version $DISTRIBUTIONS $silent.IsPresent $publish.IsPresent
